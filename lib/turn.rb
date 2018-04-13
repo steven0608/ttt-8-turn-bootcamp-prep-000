73ca7414ce128235e387d70a9273708bf61d0804
Steven Chen <ysteven.chen2@gmail.com>
2018-04-13 01:13:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,10 +7,24 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
+def input_to_index(input)
+index=input.to_i - 1
+end
+
+index=input_to_index(input)
 
 def valid_move?(board,index)
   def position_taken?(board,index)
     board[index]="X" || board[index]="O"
   end
 !position_taken?(board,index) && index<9
+end
+if valid_move?(board,index)
+    
+    
+def move(board,index,token="X")
+if valid_move?(board,index)
+    board[index]=token
+    puts display_board(board)
+  end
 end
