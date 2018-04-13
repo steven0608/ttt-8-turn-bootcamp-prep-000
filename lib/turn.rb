7822bd3e9616d62c430ede3f94b92fee89ce2ad4
Steven Chen <ysteven.chen2@gmail.com>
2018-04-13 02:01:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,9 +15,17 @@ index=2
 
 def valid_move?(board,index)
   def position_taken?(board,index)
-    board[index]="X" || board[index]="O"
+    if board[index]="X" || board[index]="O"
+      return true
+    else
+      return false
   end
-!position_taken?(board,index) && index<9
+end
+if !position_taken?(board,index) && index<9
+  return true
+else
+  return false
+end
 end
 
 
