2966bcdcef8a703d33c080907c4bda39fe8f465b
Steven Chen <ysteven.chen2@gmail.com>
2018-04-13 01:28:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,9 +7,9 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
-def input_to_index(input)
-index=input.to_i - 1
-end
+#def input_to_index(input)
+#index=input.to_i - 1
+#end
 
 
 def valid_move?(board,index)
